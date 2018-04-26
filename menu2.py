f115b59457ec8a677fd3e1f37a8200e97272acf0
Kerollos Magdy <kokomagdy98@yahoo.com>
2018-04-26 05:08:41 +0200

    add clear_screen function

--- menu2.py
+++ menu2.py
@@ -1,43 +1,49 @@
 import os
-
 import withdraw
 import deposit
 import show_history
+
+
+def clear_screen():  # function to clear the output of the screen
+    os.system('clear')
+
 
 lls = [1, 1, 1, 1500]
 
 
 def menu2():
-    os.system('clear')
-    print("\nWelcome ")
+    print("\n---------Welcome--------- ")
     ch = input("\n1) show balance \n2) show history\n3) deposit\n4) withdraw\n"
                "5) change password \n6) logout\n\nchoice>> ")
 
     if ch == 1:
-        # query
+        clear_screen()
         print("query()")
     elif ch == 2:
-        # show_history()
+        clear_screen()
         print("show_history()")
         ls = show_history.show_history(lls)
     elif ch == 3:
-        # deposit()
+        clear_screen()
         print("#deposit()")
         ls = deposit.deposit(lls)
     elif ch == 4:
-        # withdraw()
+        clear_screen()
         print("#withdraw()")
         ls = withdraw.withdraw(lls)
     elif ch == 5:
-        # change_password()
+        clear_screen()
         print("#change_pasword()")
     elif ch == 6:
-        # logout()
+        clear_screen()
         print("#logout()")
     else:
+        clear_screen()
         print("ERROR: Wrong choice\n")
         menu2()
 
     menu2()
 
 
+if __name__ == '__main__':
+    menu2()
